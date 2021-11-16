927371bf522063581b122e810d9e9e9c44999c57
unknown <john.t.mosugu@gmail.com>
2021-11-16 21:25:27 +0100

    Updated code with addition of unit test module

--- Desktop/git-Myprojects/PWP-Assignment/DataLoader.py
+++ Desktop/git-Myprojects/PWP-Assignment/DataLoader.py
@@ -36,29 +36,36 @@ class DataLoader:
         (train, ideal & test) onto the pandas DataFrames(df_train_data,df_ideal_data & df_test_data)
         """
         # Ensuring it is a virtually private constructor.
-        if DataLoader.__instance is not None:
-            raise Exception("Only singleton class allowed here!")
+        #if DataLoader.__instance is not None:
+            #raise Exception("Only singleton class allowed here!")
+        #else:
+            #DataLoader.__instance = self
+        try:
+            self.df_train_data = pd.read_csv(
+                "C:/Users/John Taiye Mosugu/Downloads/Personal stuffs/Data Science Program-IUBH/Python "
+                "Programming/Written Assignment-Datasets/train.csv")
+            self.df_ideal_data = pd.read_csv(
+                "C:/Users/John Taiye Mosugu/Downloads/Personal stuffs/Data Science Program-IUBH/Python "
+                "Programming/Written Assignment-Datasets/ideal.csv")
+            self.df_test_data = pd.read_csv(
+                "C:/Users/John Taiye Mosugu/Downloads/Personal stuffs/Data Science Program-IUBH/Python "
+                "Programming/Written Assignment-Datasets/test.csv")
+        except Exception as e:
+            print("Exception while loading dataframe :", e)
+            sys.exit(1)
         else:
-            DataLoader.__instance = self
-            try:
-                self.df_train_data = pd.read_csv(sys.argv[1])
-                self.df_ideal_data = pd.read_csv(sys.argv[2])
-                self.df_test_data = pd.read_csv(sys.argv[3])
-            except Exception as e:
-                print("Exception while loading dataframe :", e)
+            if self.df_train_data.empty:
+                print("Empty training dataset encountered")
+                sys.exit(1)
+            elif self.df_ideal_data.empty:
+                print("Empty Ideal dataset encountered")
+                sys.exit(1)
+            elif self.df_test_data.empty:
+                print("Empty Test dataset encountered")
                 sys.exit(1)
             else:
-                if self.df_train_data.empty:
-                    print("Empty training dataset encountered")
-                    sys.exit(1)
-                elif self.df_ideal_data.empty:
-                    print("Empty Ideal dataset encountered")
-                    sys.exit(1)
-                elif self.df_test_data.empty:
-                    print("Empty Test dataset encountered")
-                    sys.exit(1)
-                else:
-                    print("Data loaded to dataframe(s) successfully...")
+                print("Data loaded to dataframe(s) successfully...")
+
 
     def retrieve_train_data(self):
         """
--- Desktop/git-Myprojects/PWP-Assignment/testpwp.py
+++ Desktop/git-Myprojects/PWP-Assignment/testpwp.py
@@ -1,39 +1,52 @@
 import unittest
 from DataLoader import DataLoader
-import TrainData
-import TestData
-import GeneralPurposeRoutine
-import CurveAdjusting
-from DataLoaderX import DataLoaderX
+import pandas as pd
+
+from TestData import TestData
+from TrainData import TrainData
 
 
-class TestDataLoader(unittest.TestCase):
+class TestDataShapes(unittest.TestCase):
 
-    def testgetInstance(self):
-        var_xx =TrainData()
-        var_xx1 = var_xx.train_model()
-        self.assertIsInstance(var_xx1,TrainData(), "Not an instance of this class")
+    def setUp(self):
+        var_data = DataLoader()
+        self.train_data = var_data.retrieve_train_data()
+        self.ideal_data = var_data.retrieve_ideal_data()
+        self.test_data = var_data.retrieve_test_data()
+
+    def testCheckTrainDataShape(self):
+        returned_shape = pd.DataFrame(self.train_data).shape
+        expected_shape = (400, 5)
+        self.assertEqual(returned_shape, expected_shape, "Train data is of the wrong dimention/shape")
+
+    def testCheckTestDataShape(self):
+        returned_shape = pd.DataFrame(self.test_data).shape
+        expected_shape = (100, 2)
+        self.assertEqual(returned_shape, expected_shape, "Test data is of the wrong dimention/shape")
+
+    def testCheckIdealDataShape(self):
+        returned_shape = pd.DataFrame(self.ideal_data).shape
+        expected_shape = (400, 51)
+        self.assertEqual(returned_shape, expected_shape, "Ideal data is of the wrong dimention/shape")
 
 
+class TestTrainTestResults(unittest.TestCase):
 
+    def setUp(self):
+        self.var_train = TrainData()
+        self.var_train_results = self.var_train.train_model()
+        self.var_test = TestData()
+        self.var_test_results = self.var_test.test_model(self.var_train_results)
 
-class TestTrainData(unittest.TestCase):
-    def test_deviation(self):
-        pass
+    def test_TestTrainResults(self):
+        expected_results= {'y1', 'y2', 'y3', 'y4'}
+        return_results = self.var_train_results.keys()
+        self.assertEqual(return_results,expected_results,"Train Results is not of the  same shape")
 
-
-class TestTestData(unittest.TestCase):
-    def test_deviation(self):
-        pass
-
-class TestGeneralPurposeRoutine(unittest.TestCase):
-    def test_deviation(self):
-        pass
-
-
-class TestCurveAdjusting(unittest.TestCase):
-    def test_deviation(self):
-        pass
+    def test_TestResults(self):
+        expected_results = 50
+        return_results = len(self.var_test_results)
+        self.assertEqual(return_results, expected_results, "Test Results has wrong size")
 
 
 if __name__ == '__main__':
